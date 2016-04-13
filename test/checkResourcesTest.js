611950dda9850e1df5fa40bfd2b2491f550360e8
rowan <rowanu@gmail.com>
2016-04-13 16:04:22 +1000

    Include the resource's name if it fails a type check.

--- test/checkResourcesTest.js
+++ test/checkResourcesTest.js
@@ -12,14 +12,28 @@ tap.test('areValid with valid template', function (t) {
 });
 
 tap.test('areValid with invalid resource type', function (t) {
-  t.plan(2);
+  t.plan(3);
   var template = require('./template.json');
   var invalidType = template.Resources.ElasticLoadBalancer.Type = 'AWS::Not::A::Resource';
   return checkResources.areValid(template).then(function (errors) {
     var error = errors[0];
     t.equal(errors.length, 1, 'has one error');
-    t.match(error, new RegExp(invalidType), 'has the invalid property\'s name');
+    t.match(error, /ElasticLoadBalancer/, 'has the invalid Resource\'s name');
+    t.match(error, new RegExp(invalidType), 'has the invalid type\'s name');
   });
 });
 
 // tap.test('areValid with invalid resource property', function (t) {
+//   t.plan(2);
+//   var template = require('./template.json');
+//   var invalidProperty = template.Resources.ElasticLoadBalancer.Type = 'NotAProperty';
+//   return checkResources.areValid(template).then(function (errors) {
+//     var error = errors[0];
+//     t.equal(errors.length, 1, 'has one error');
+//     t.match(error, new RegExp(invalidProperty), 'has the invalid property\'s name');
+//   });
+// });
+
+// TODO: work out required properties
+// TODO: allow optional properties or not
+// TODO: Conditional properties (!)
