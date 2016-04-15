b4902b23e02cd44826f2ef14f5a5ed0fc79d7a30
rowan <rowanu@gmail.com>
2016-04-15 15:51:42 +1000

    Update script to support pretty or compact output.

--- bin/cfn-check.js
+++ bin/cfn-check.js
@@ -13,8 +13,27 @@ program
   .option('-c, --compact', 'Print compact JSON when valid (regardless of character limit)')
   .parse(process.argv);
 
-if (program.args.length < 1) { program.help(); }
-if (program.watch) { console.log(`Watching ${program.args}... (not really)`); }
+  if (program.args.length < 1) { program.help(); }
+  if (program.watch) {
+    console.log(`Watching ${program.args}... (not really)`);
+    var options = {
+      'env': {
+        'NODE_ENV': 'development'
+      },
+      'legacyWatch': true,
+      'restartable': 'rs',
+      'script': './bin/check.js',
+      'stdout': true,
+      'verbose': false,
+      'watch': [
+        program.args[0]
+      ]
+  }
+}
+var spacer = 2; // Default to pretty
+// TODO: Change printing option depending on length of template
+if (program.pretty) { spacer = 2; }
+if (program.compact) { spacer = null; }
 
 var parse = require('../lib/parse.js');
 var check = require('../lib/check');
@@ -29,7 +48,7 @@ parse(fs.readFileSync(program.args[0], 'utf8')).then(function (template) {
         console.log(errors.join('\n'));
         process.exit(1);
       } else {
-        console.log(JSON.stringify(template, null, 2));
+        console.log(JSON.stringify(template, null, spacer));
         process.exit(0);
       }
     });
