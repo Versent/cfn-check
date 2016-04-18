f0d4ea6b2559984db7270dad63c564c6a57a6a94
rowan <rowanu@gmail.com>
2016-04-18 10:56:14 +1000

    Make --watch command work (needs clean-up).

--- bin/cfn-check.js
+++ bin/cfn-check.js
@@ -4,6 +4,7 @@ var _ = require('lodash');
 var fs = require('fs');
 var Promise = require('bluebird');
 var program = require('commander');
+var nodemon = require('nodemon');
 
 program
   .version('1.0.0')
@@ -13,23 +14,7 @@ program
   .option('-c, --compact', 'Print compact JSON when valid (regardless of character limit)')
   .parse(process.argv);
 
-  if (program.args.length < 1) { program.help(); }
-  if (program.watch) {
-    console.log(`Watching ${program.args}... (not really)`);
-    var options = {
-      'env': {
-        'NODE_ENV': 'development'
-      },
-      'legacyWatch': true,
-      'restartable': 'rs',
-      'script': './bin/check.js',
-      'stdout': true,
-      'verbose': false,
-      'watch': [
-        program.args[0]
-      ]
-  }
-}
+if (program.args.length < 1) { program.help(); }
 var spacer = 2; // Default to pretty
 // TODO: Change printing option depending on length of template
 if (program.pretty) { spacer = 2; }
@@ -38,22 +23,40 @@ if (program.compact) { spacer = null; }
 var parse = require('../lib/parse.js');
 var check = require('../lib/check');
 
-parse(fs.readFileSync(program.args[0], 'utf8')).then(function (template) {
-  return Promise.join(
-    check.references.areValid(template),
-    check.resources.areValid(template),
-    function (referencesErrors, resourcesErrors) {
-      var errors = _.concat(referencesErrors, resourcesErrors);
-      if (errors.length > 0) {
-        console.log(errors.join('\n'));
-        process.exit(1);
-      } else {
-        console.log(JSON.stringify(template, null, spacer));
-        process.exit(0);
-      }
-    });
-})
-.catch(function (error) {
-  console.log(error.message);
-  process.exit(1);
-});
+if (program.watch) {
+  console.log(`Watching ${program.args}...`);
+  var options = {
+    'env': {
+      'NODE_ENV': 'development'
+    },
+    'restartable': 'rs',
+    'exec': 'cfn-check ' + program.args[0],
+    'stdout': true,
+    'verbose': false,
+    'quiet': true,
+    'watch': [
+      program.args[0]
+    ]
+  }
+  nodemon(options);
+} else {
+  parse(fs.readFileSync(program.args[0], 'utf8')).then(function (template) {
+    return Promise.join(
+      check.references.areValid(template),
+      check.resources.areValid(template),
+      function (referencesErrors, resourcesErrors) {
+        var errors = _.concat(referencesErrors, resourcesErrors);
+        if (errors.length > 0) {
+          console.log(errors.join('\n'));
+          process.exit(1);
+        } else {
+          console.log(JSON.stringify(template, null, spacer));
+          process.exit(0);
+        }
+      });
+  })
+  .catch(function (error) {
+    console.log(error.message);
+    process.exit(1);
+  });
+}
